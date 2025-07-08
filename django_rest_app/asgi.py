1cb0a2388f65ff82bb06d881b171a98690815b45
khanhnp <khanhnp.ot@gmail.com>
2025-07-08 11:31:38 +0700

    Add CORS package to REST

--- django_rest_app/asgi.py
+++ django_rest_app/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for djangorestapp project.
+ASGI config for django_rest_app project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangorestapp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_rest_app.settings')
 
 application = get_asgi_application()
--- django_rest_app/urls.py
+++ django_rest_app/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for djangorestapp project.
+URL configuration for django_rest_app project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
